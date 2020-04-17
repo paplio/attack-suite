1b796923ec64861c4c3558c00d39fdf9348a8604
paplio <ani.12.28.98@gmail.com>
2020-04-17 22:22:53 +0530

    Add files via upload

--- flaskSQL.py
+++ flaskSQL.py
@@ -1,22 +1,27 @@
 import sqlite3
 from flask import Flask, render_template, redirect, url_for, request
 
-cursor = sqlite3.connect('database.db') #connect to the database that we'll be using for our website
+app = Flask(__name__)
 
 #just gonna go ahead and add some dummy thingys for now, since we're really just looking to exploit this data and read/edit/delete it
 
-cursor.execute('CREATE table usernames')
-
+#SELECT exists(SELECT * from usernames where name = usr
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+	cursor = sqlite3.connect('database.db') #connect to the database that we'll be using for our website
 	if request.method == 'POST': #this meeans the user is trying to enter a username+password into our form! yay! or dun dun dunn is it a hacker
-		name = request.form['name']
+		usr = request.form['name']
 		password = request.form['password']
-		if(cursor.execute('SELECT exists(SELECT * from names where name = ?', (name,)):
-			return render_template('success.html')
-	else return render_template('nope.html')
+		commandThing = "SELECT * from usernames where name like'"+usr+"'"
+		for i in cursor.execute(commandThing):
+			print i
+		#if(cursor.execute(commandThing).fetchone()[0]):
+		#	return "yep"
+	else: return render_template('login.html')
 
+if __name__ == '__main__':
+   app.run()
 
 
 
